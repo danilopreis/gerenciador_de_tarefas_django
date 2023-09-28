417003bc15e4439b4b4786d54e35d99fafdbf34c
Danilo Reis <danilop114@gmail.com>
2023-09-28 15:12:18 -0300

    added btns in modal and form in btn Editar

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,7 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Tarefa
+from .forms import AdicionarTarefa, EditarTarefaForm
 
 # Create your views here.
 def tarefas_pendentes_list(request):
     tarefas_pendentes = Tarefa.objects.filter(status='pendente')
-    return render(request, 'tarefas/tarefas_pendentes.html', {'tarefas_pendentes':tarefas_pendentes})
+    if request.method == "POST":
+        form = AdicionarTarefa(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('tarefas_pendentes_list')
+    else:
+        form = AdicionarTarefa()
+    
+    return render(request, 'tarefas/tarefas_pendentes.html', {'tarefas_pendentes':tarefas_pendentes, 'form':form})
+
+
+def concluir_tarefa(request, tarefa_id):
+    tarefa = get_object_or_404(Tarefa, id=tarefa_id)
+    tarefa.status = 'concluido'
+    tarefa.save()
+    return redirect('tarefas_pendentes_list')
+
+def excluir_tarefa(request, tarefa_id):
+    tarefa = get_object_or_404(Tarefa, id=tarefa_id)
+    tarefa.delete()
+    return redirect('tarefas_pendentes_list')
+
+def adiar_tarefa(request, tarefa_id):
+    tarefa = get_object_or_404(Tarefa, id=tarefa_id)
+    tarefa.status = 'adiado'
+    tarefa.save()
+    return redirect('tarefas_pendentes_list')
+
+def editar_tarefa(request, tarefa_id):
+    tarefa = get_object_or_404(Tarefa, id=tarefa_id)
+    if request.method == "POST":
+        form = EditarTarefaForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            tarefa.descricao = cd['tarefa']
+            tarefa.categoria = cd['categoria']
+            tarefa.save()
+            return redirect('tarefas_pendentes_list')
+    else:
+        form = EditarTarefaForm(initial=('tarefa':tarefa.descricao, 'categoria':tarefa.categoria))
+    return render(request, 'tarefas/editar_tarefa.html', {'tarefa':tarefa, 'form':form})
